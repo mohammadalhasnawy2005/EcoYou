3cc01c174b519ae5c16ded54cff1acdd75d6ccdc
mohammadalhasnawy2005 <157133345+mohammadalhasnawy2005@users.noreply.github.com>
2025-07-25 17:47:48 +0300

    fix sharedPref the eroor was cuse a litter of onboarding was onBorading so was not save now work

--- lib/controller/onboardingcontroller.dart
+++ lib/controller/onboardingcontroller.dart
@@ -5,8 +5,8 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 abstract class OnBoardingController extends GetxController {
-  next();
-  onPageChanged(int index);
+  void next();
+  void onPageChanged(int index);
 }
 
 class OnBoardingControllerImp extends OnBoardingController {
@@ -18,7 +18,7 @@ class OnBoardingControllerImp extends OnBoardingController {
   next() {
     currentPage++;
     if (currentPage > onBoardingList.length - 1) {
-      myService.sharedPreferences.setString("onBoarding", "1");
+      myService.sharedPreferences.setString("onboarding", "1");
       Get.offAllNamed(AppRoute.login);
     } else {
       pageController.animateToPage(
